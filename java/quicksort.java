d846fc1a036cceac6de756708d9df168497227f6
Draksha <96646429+Draksha-the-decoder@users.noreply.github.com>
2024-09-23 22:19:56 +0530

    quick sort

--- java/quicksort.java
+++ java/quicksort.java
@@ -2,18 +2,19 @@ import java.util.*;
 class quicksort{
     public static void main(String args[])
     {
-        int arr[]={3,1,2,1,4,5,6,4};
+        int arr[]={3,1,2,4,1,5,2,6,4};
         sort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
     static void sort(int[] arr,int start,int end)
     {
-        if(start>=end) return;
+        if(start<end){
         int partition = partition(arr,start,end);
+        System.out.println("sort "+Arrays.toString(arr)+" "+partition);
         sort(arr,start,partition-1);
         sort(arr,partition+1,end);
-        System.out.println("sort "+Arrays.toString(arr)+" "+partition);
+        }        
     }
     static int partition(int[] arr,int start,int end)
     {   
@@ -28,12 +29,15 @@ class quicksort{
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
+                System.out.println("partition "+Arrays.toString(arr)+" "+i+" "+j);
             }
-            else break;
+          
         }         
         int temp = arr[pivot];   
         arr[pivot]=arr[j];
         arr[j]=temp;
-        return j;       
+        System.out.println("partition "+Arrays.toString(arr)+" exchange with pos "+j+" range "+start+" to "+end);
+        return j;      
+         
     }
 }
