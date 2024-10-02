d796f3e847253de8d3fc01877ec94e5a70d92e01
Draksha <96646429+Draksha-the-decoder@users.noreply.github.com>
2024-10-02 21:54:59 +0530

    add fraction

---
+++ java/addfrac.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class addfrac{
+    public static void main(String args[])
+    {
+       int n1=4,d1=5,n2=6,d2=9;
+       int LCM = Math.max(d1,d2);
+       while(!((LCM % d1==0) && (LCM % d2==0)))
+       LCM++;       
+       System.out.println("denomi "+LCM);
+       System.out.println("numer "+((n1*(LCM/d1))+(n2*(LCM/d2))));  
+    }
+}
